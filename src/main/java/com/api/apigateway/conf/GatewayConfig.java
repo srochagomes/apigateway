47d44fa7af4af06f3653752c5e4b45289bed5223
Sérgio Gomes <srochg@gmail.com>
2023-08-02 21:56:46 -0300

    Configuration for API

--- src/main/java/com/api/apigateway/conf/GatewayConfig.java
+++ src/main/java/com/api/apigateway/conf/GatewayConfig.java
@@ -1,14 +1,10 @@
 package com.api.apigateway.conf;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.cors.reactive.CorsUtils;
-import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -20,7 +16,7 @@ import java.util.Arrays;
 //@Configuration
 public class GatewayConfig {
 
-  //  @Bean
+    //@Bean
     public WebFilter corsFilter() {
         return (ServerWebExchange exchange, WebFilterChain chain) -> {
             ServerHttpRequest request = exchange.getRequest();
